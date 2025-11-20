e9e3695ed048b36b389852ff7bb489576e64d66a
RalphHellisng62 <arenasrafael889@gmail.com>
2025-11-19 21:55:15 -0600

    Construcción de main

--- MatrizAdjunta.java
+++ MatrizAdjunta.java
@@ -1,3 +1,29 @@
 public class MatrizAdjunta {
-    
+    // iniciamos construyendo un main
+    public static void main(String[] args) {
+        String archivoEntrada = "matriz.txt";
+        String archivoSalida = "matriz_adjunta.txt";
+
+        double[][] matriz = leerMatriz(archivoEntrada);
+
+        if (matriz != null) {
+            System.out.println("matriz Original:");
+            imprimirMatriz(matriz);
+
+            double[][] adjunta = matrizAdjunta(matriz);
+
+            if (adjunta != null) {
+                System.out.println("\n Matriz Adjunta:");
+                imprimirMatriz(adjunta);
+
+                escribirArchivo(adjunta, archivoSalida);
+                System.out.println("\n archivo generado: " + archivoSalida);
+            } else {
+                System.out.println("\n la matriz no es cuadrada. No se puede calcular la adjunta.");
+            }
+        } else {
+            System.out.println("error al leer el archivo.");
+        }
+    }
+
 }
