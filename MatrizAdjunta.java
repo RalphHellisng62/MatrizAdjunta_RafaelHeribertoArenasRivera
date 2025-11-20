15c79c0612389725a9370bf583812b3206e40852
RalphHellisng62 <arenasrafael889@gmail.com>
2025-11-19 21:59:30 -0600

    lee el archivo

--- MatrizAdjunta.java
+++ MatrizAdjunta.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class MatrizAdjunta {
     // iniciamos construyendo un main
     public static void main(String[] args) {
@@ -23,6 +27,41 @@ public class MatrizAdjunta {
             }
         } else {
             System.out.println("error al leer el archivo.");
+        }
+    }
+
+     // lee la matriz desde el archivo
+    public static double[][] leerMatriz(String nombreArchivo) {
+        try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+            int filas = 0;
+
+            // contara las filas
+            while ((linea = br.readLine()) != null) {
+                filas++;
+            }
+
+            double[][] matriz = new double[filas][];
+            br.close();
+
+            // lo lee de nuevo para guardar datos
+            BufferedReader br2 = new BufferedReader(new FileReader(nombreArchivo));
+            int i = 0;
+            while ((linea = br2.readLine()) != null) {
+                String[] valores = linea.trim().split(" ");
+                matriz[i] = new double[valores.length];
+                for (int j = 0; j < valores.length; j++) {
+                    matriz[i][j] = Double.parseDouble(valores[j]);
+                }
+                i++;
+            }
+            br2.close();
+
+            return matriz;
+
+        } catch (IOException | NumberFormatException e) {
+            System.out.println("error al leer archivo: " + e.getMessage());
+            return null;
         }
     }
 
